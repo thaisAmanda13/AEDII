0a2603cef75af4d15628a5f64bf8a707ddcf4b13
Thais Amanda <thais.amandas18@gmail.com>
2019-08-24 08:57:31 -0300

    arvore binaria inclusa

--- bibliotecas/ArvoreBinaria.c
+++ bibliotecas/ArvoreBinaria.c
@@ -1,58 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct noTreeTag {
-    int info;
-    struct noTreeTag *esquerda;
-    struct noTreeTag *direita;
-}noArvore;
-void iniciaArvore(noArvore ** Tree){
-        (*Tree) = NULL;
-}
-int insereABB(noArvore **Tree, int elemento){
-    if(*Tree == NULL){
-        *Tree = malloc(sizeof(noArvore));
-        if(*Tree != NULL){
-            (*Tree)->info = elemento;
-            (*Tree)->esquerda = NULL;
-            (*Tree)->direita = NULL;
-            return 1;// deu certo a inserção
-        }
-        else{
-            return 2;// nao havia memoria para alocar entao nao foi possivel inserir
-        }
-    }
-    else{
-        if(elemento < (*Tree)->info)
-            insereABB(&((*Tree)->esquerda), elemento);
-        else{
-            if(elemento > (*Tree)->info)
-                insereABB(&((*Tree)->direita), elemento);
-            else
-                return 3; // valor repetido nao é permitido por isso nao insere
-        }
-    }
-
-}
-
-// Exemplo de função – Exibe nós da ABB
-void ExibeOrdem_AB(noArvore **T) {
-    if(*T != NULL) {
-    ExibeOrdem_AB(& ((*T)->esquerda));
-    printf("\t |%d|", (*T)->info);
-    ExibeOrdem_AB(& ((*T)->direita));
-    }
-}
+#include "ArvoreBinaria.h"
 int main(){
     noArvore *arvore;
     iniciaArvore(&arvore);
-    insereABB(&arvore, 15);
-    insereABB(&arvore, 18);
-    insereABB(&arvore, 11);
-    insereABB(&arvore, 1);
-    insereABB(&arvore, 19);
-       
-       
-    ExibeOrdem_AB(&arvore);
+    insereABB(&arvore, 10);
+    insereABB(&arvore, 8);
+    insereABB(&arvore, 20);
+    insereABB(&arvore,2 );
+    insereABB(&arvore, 9);
+    insereABB(&arvore, 30);
+    printf("\n\n");  
+    exibeFolhas(&arvore);
     system("pause");
+    exibeFolhas(&arvore);
     return 0;
 }
--- bibliotecas/ListaEncadeada.h
+++ bibliotecas/ListaEncadeada.h
@@ -1,111 +1,132 @@
 #include<stdio.h>
-#include<math.h>
 #include<stdlib.h>
-#include<string.h>
 
-#define TAM 4 //numero aleatorio, pode mudar kskssk
-typedef struct noPalavra{
-    int qtdVezes ;
-    char valor[100]; //texto de ate 500 letras
-    struct noPalavra *link;
-   
-}noStr;
+typedef struct noTag{
+    int valor;
+    struct noTag *link;
+}no;
 
-int compararPalavras(char vetor[], char vetor2[]){
-    for(int i = 0; vetor[i] != '\0' && vetor2[i] != '\0' && vetor[i] != ' ' && vetor2[i] != ' '; i++){
-        if(vetor[i] != vetor2[i]){
-            printf("%c %c ", vetor[i], vetor2[i]);
-            return 1;
-        }
+
+void iniciaLista(no **Lista);
+void imprime(no *Lista);
+void insereInicio(no **Lista, int num);
+int removeInicio(no **Lista);
+void insereFinal(no **Lista, int num);
+int removeFinal(no **Lista);
+void insereOrdenada(no **Lista,int num);
+
+
+void insereOrdenada(no **Lista,int num){
+   no *proximo, *novoNo;
+	novoNo = (no *)malloc(sizeof(no));
+	novoNo->valor = num;
+
+	if (*Lista == NULL)
+	{
+		*Lista = novoNo;
+		novoNo->link = NULL;
+		return;
+	}
+
+	proximo = *Lista;
+
+	if (proximo->valor > num)
+	{
+		novoNo->link = proximo;
+		*Lista = novoNo;
+		return;
+	}
+
+	while (proximo->link != NULL && proximo->link->valor < num)
+	{
+		proximo = proximo->link;
+	}
+	novoNo->link = proximo->link;
+	proximo->link = novoNo;
+}
+
+void iniciaLista(no **Lista){
+    *Lista = NULL;   
+}
+
+void imprime(no *Lista){
+    if(Lista ==  NULL){
+        printf("A lista esta vazia!\n");
     }
-        return 0;
+    while(Lista != NULL){
+        printf("|%d|\t",Lista->valor);
+        Lista = Lista->link;
+    }
 }
 
-void exibeConteudo(char Palavra[]){
-    for(int x = 0; Palavra[x] != '\0'; x++)
-        printf("%c ",Palavra[x]);
-}
-
-void insereInicioString(noStr **Lista, char Palavra[]){
-     noStr *novoNO;
-    novoNO = malloc(sizeof(noStr));
-    
-    strcpy(novoNO->valor, Palavra); //copia palavra para valor
-    if(*Lista == NULL){
+void insereInicio(no **Lista, int num){
+    no *novoNO;
+    novoNO = malloc(sizeof(no));
+    novoNO->valor = num;
+    if(*Lista == NULL)
         novoNO->link = NULL;
-        *Lista = novoNO;
-    }  
+    else
+        novoNO->link = *Lista;
+    *Lista = novoNO;       // lembrar de conferir essa linha 
 }
-int inserirPalavra(noStr **Classe, char Palavra[], int Hash){
-    noStr *aux;
-    aux = *Classe;
-    printf("ENTREI\n");
-    // if (aux == NULL){
-    //     (*Classe)->qtdVezes = 0;
-    // }
-
-    printf("passei if");
-    while(aux != NULL){
-        printf("Entrei while\n");
-        if (compararPalavras(aux->valor ,Palavra) == 0){//se a palavra for igual incrementa
-            printf("entrei if\n");
-            //aux->qtdVezes ++;
-            return 0;
-        }
-        aux = aux->link;
-    }//senao tem nenhum igual inserir o nó no Hash correspondente
-    printf("Vou inserir \n");
-    system("pause");
-    insereInicioString(Classe, Palavra);
-}
-int calcularHashTexto_Insere(char Texto[], noStr **Classes){
- 
-    int PosicaoAtual = 0, x = 0, cont2 = 0;
-   
-        while(Texto[PosicaoAtual] != '\0'){
-        
-           
-            int soma = 0;
-            for(x = PosicaoAtual ; Texto[x] != ' ' && Texto[x] != '\0'; x ++){
-                printf("%c ",Texto[x]);
-                soma += Texto[x];
-            }
-            printf("\n\n soma %d",soma);
-            printf(" \n");
-           inserirPalavra(Classes, Texto, soma % TAM);
-           printf("inseri e vortei ");
-            PosicaoAtual = x + 1; 
-            
-           
-        }
-        printf("Sai do for");
-        system("pause");
-}
-int main(){
-    char texto[]={'t','e','x','t','o',' ','a','l','e','a','t','o','r','i','o',' ','p','a','r','a',' ','t','e','s','t','e'};
-    noStr *Classes[10]; 
-    noStr *aux = Classes[0];
+int removeInicio(no **Lista){
+    int valorRemovido;
     
-    for(int i = 0; i < 10; i ++)
-        Classes[i] = NULL;
-
-    system("pause");
-    calcularHashTexto_Insere(texto, Classes);
-    
-
-    for(int j = 0; j < 10; j++ ){
-        printf("entrei primeiro for\n");
-        for(int i  = 0; i < 6; i++){ 
-            while(aux != NULL){
-                printf("Passei while \n");
-               // printf("\nA palavra %c\n",aux->valor[i]);
-                aux = aux->link;
-                 printf("passei aux \n");
-            }
+    if(*Lista != NULL){
+        if((*Lista)->link == NULL){ // caso tenha um unico nozinho
+         no *aux = *Lista;
+            valorRemovido = aux->valor;
+            free(*Lista);
+            *Lista = NULL;
+        } 
+        else{
+            no *auxLista;
+            auxLista = *Lista;
+            valorRemovido = auxLista->valor;
+            *Lista = auxLista->link;
+            free(auxLista);
         }
-        aux = Classes[j];
+        return valorRemovido;
     }
-        system("pause");
+    else
+        return -1; // lista vazia.
+    
+}
+void insereFinal(no **Lista, int num){
+    no *prox, *novoNO;
+    novoNO = malloc(sizeof(no));
+    novoNO->valor = num;
+    novoNO->link = NULL;
+    if(*Lista == NULL)
+        *Lista = novoNO;
+    else{
+        prox = *Lista;
+        while(prox->link != NULL){
+            prox = prox->link;
+        }
+        prox->link = novoNO;
+    }
+}
+int removeFinal(no **Lista){
+    no *prox, *anterior;
+    int valorRemovido;
+    if(*Lista != NULL){
+        if((*Lista)->link == NULL){
+            valorRemovido = (*Lista)->valor;
+            free(*Lista);
+            *Lista = NULL;
+        }
+        else{
+            prox = *Lista;
+            while(prox->link != NULL){
+                anterior = prox;
+                prox = prox->link;
+            }
+            valorRemovido = prox->valor;
+            anterior->link = NULL;
+            free(prox);
+        }
+        return valorRemovido;
+    }
 
-printf("\n\nsai\n");
-system("pause");}
+}
